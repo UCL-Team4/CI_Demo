61e9470bcad9cae312acfb7f8dc9f8105c46cab9
gabikucharska <gaku62308@edu.ucl.dk>
2025-04-10 14:23:26 +0200

    Whatever

--- CI_Demo/ICalculator.cs
+++ CI_Demo/ICalculator.cs
@@ -12,5 +12,7 @@ namespace CI_Demo
         int Subtract(int a, int b);
         int Multiply(int a, int b);
         double Divide(int a, int b);
+        double Power(double a, double b);
+        double SquareRoot(double a);
     }
 }
--- CI_Demo/Program.cs
+++ CI_Demo/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Numerics;
+using CI_Demo;
+
+Calculator calculator = new Calculator();
+
+Console.WriteLine("Welcome to our calculator!");
+
+Console.WriteLine("Chose a function");
+Console.WriteLine("For addition press A");
+Console.WriteLine("For subtraction press B");
+Console.WriteLine("For division press C");
+Console.WriteLine("For multiplication press D");
+Console.WriteLine("For squareroot press E");
+Console.WriteLine("For powers press F");
+
+string choice = Console.ReadLine();
+switch (choice)
+
+{
+	case "A": Addition();
+		break;
+	default:
+		break;
+}
+
+void Addition()
+{
+	Console.WriteLine("Type your first number:");
+	string FirstNumber = Console.ReadLine();
+
+	Console.WriteLine("Type your secoond number:");
+    string SecondNumber = Console.ReadLine();
+
+	int Answer = calculator.Add(int.Parse(FirstNumber), int.Parse(SecondNumber));
+	Console.WriteLine($"The answer is {Answer}"); 
+}
+
+
+
